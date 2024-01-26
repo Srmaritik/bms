a2e3cc2bfea43fc3141b889610f6dbf6ea361a8d
Srmaritik <rozanchapagain3@gmail.com>
2024-01-26 18:17:33 +0545

    add

---
+++ bankManagementSystem/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	//	IDandPasswords idAndPasswords = new IDandPasswords();
+		
+		LoginPage loginPage = new LoginPage();
+		
+	//	SignUp2 signUp2 = new SignUp2();
+		
+	//	WelcomePage welcomePage = new WelcomePage("hey");
+	//	AccDetails acc = new AccDetails("");
+	}
+
+}
